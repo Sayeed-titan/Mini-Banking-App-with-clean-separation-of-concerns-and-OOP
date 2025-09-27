f7f0f57e8fd8823ddb3b75c3b1c037e1f5c39928
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-09-28 00:09:57 +0600

    Add Interface ITransaction

---
+++ BankSystem.ConsoleApp/Core/Interfaces/ITransaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.ConsoleApp.Core.Interfaces
+{
+    public interface ITransaction
+    {
+        void Deposite(decimal amount, string? description = null);
+        void Withdraw(decimal amoint, string? description = null);
+        decimal GetBalance();
+    }
+}
