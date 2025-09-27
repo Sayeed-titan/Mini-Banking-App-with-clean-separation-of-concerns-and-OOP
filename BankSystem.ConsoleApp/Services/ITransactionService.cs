0dc3738078090af999bd2f47ff99ebf661a294cc
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-09-28 01:11:41 +0600

    Add ITransaction Service

---
+++ BankSystem.ConsoleApp/Services/ITransactionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace BankSystem.ConsoleApp.Services
+{
+    public interface ITransactionService
+    {
+        void RecordTransaction(Transaction tx);
+        IEnumerable<Transaction> GetTransactionsForAccount(string accountNumber);
+        IEnumerable<Transaction> GetAllTransactions();
+    }
+}
