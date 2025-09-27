12f405adaae6190e9b948afbefec8cb909b0575a
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-09-28 00:53:27 +0600

    Add IAccountService

---
+++ BankSystem.ConsoleApp/Services/IAccountService.cs
@@ -0,0 +1,18 @@
+﻿using BankSystem.ConsoleApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.ConsoleApp.Services
+{
+    public interface IAccountService
+    {
+        Account CreateSavings(string accountNumber, string ownerName, decimal initialBalance = 0m);
+        Account CreateChecking(string accountNumber, string ownerName, decimal initialBalance = 0m, decimal overdraftLimit = 500m);
+        Account? GetByAccountNumber (string accountNumber);
+        IEnumerable<Account> GetAccounts();
+
+    }
+}
