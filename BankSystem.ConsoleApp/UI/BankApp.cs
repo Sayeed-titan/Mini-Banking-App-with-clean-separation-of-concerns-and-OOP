89364aa0c718e2b5675c8b779f64a94faf61f6b6
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-09-28 01:39:29 +0600

    Add BankApp UI

---
+++ BankSystem.ConsoleApp/UI/BankApp.cs
@@ -0,0 +1,31 @@
+﻿using BankSystem.ConsoleApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.ConsoleApp.UI
+{
+    public class BankApp
+    {
+        private readonly IAccountService _accountService;
+        private readonly ITransactionService _transactionService;
+
+        public BankApp()
+        {
+            _accountService = new AccountService();
+            _transactionService = new TransactionService();
+
+
+            //seed demo accounts 
+            _accountService.CreateSavings("S1001", "Sayeed", 1000m);
+            _accountService.CreateChecking("C2001", "Habib", 200m, overdraftLimit: 300m);
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Test Run");
+        }
+    }
+}
