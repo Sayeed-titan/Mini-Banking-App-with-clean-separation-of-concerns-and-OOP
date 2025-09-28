c99af989b07982fec88688191f9e81f2b9e1b23f
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-09-28 17:40:39 +0600

    Add Role and User entity

---
+++ BankSystem.ConsoleApp/Core/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using BankSystem.ConsoleApp.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.ConsoleApp.Core.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public Role Role { get; set; } = Role.Customer;
+
+        public override string ToString() => $"{Username} ({Role})";
+    }
+}
