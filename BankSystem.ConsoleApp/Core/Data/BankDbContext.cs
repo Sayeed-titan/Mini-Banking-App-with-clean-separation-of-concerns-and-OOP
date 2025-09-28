0e6871123cc4ae20e8f5d8ea98ab89e6dbd8be5a
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-09-28 12:16:11 +0600

    Add DbContext options in the Program.cs

---
+++ BankSystem.ConsoleApp/Core/Data/BankDbContext.cs
@@ -0,0 +1,33 @@
+﻿using BankSystem.ConsoleApp.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.ConsoleApp.Core.Data
+{
+    public class BankDbContext : DbContext
+    {
+        public BankDbContext(DbContextOptions<BankDbContext> options) : base(options)
+        {
+          
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Core.Models.Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Account>()
+                .HasDiscriminator<string>("AccountType")
+                .HasValue<SavingsAccount>("Savings")
+                .HasValue<CheckingAccount>("Checking");
+
+        }
+
+    }
+}
--- BankSystem.ConsoleApp/Program.cs
+++ BankSystem.ConsoleApp/Program.cs
@@ -1,6 +1,17 @@
-﻿using BankSystem.ConsoleApp.UI;
+﻿using BankSystem.ConsoleApp.Core.Data;
+using BankSystem.ConsoleApp.UI;
+using Microsoft.EntityFrameworkCore;
+
+var options = new DbContextOptionsBuilder<BankDbContext>()
+    .UseSqlServer("Server =localhost;Database=BankConsoleDB;Trusted_Connection=True;TrustServerCertificate=True;").Options;
+
+using var context = new BankDbContext(options);
+
+//Make sure database exists
+context.Database.EnsureCreated();
 
 var app = new BankApp();
 app.Run();
 
 Console.ReadKey();
+
